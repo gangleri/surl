c38d0c5f88a7f76f0b6cb9c76f3aeae286fcb8ce
gangleri <alan@gangleri.net>
2013-09-16 21:31:28 +0100

    Bijection module to replace bas64, this allows the alphabet to be supplied, default is for 64

---
+++ test/bijection_encode.js
@@ -0,0 +1,27 @@
+var base = require('../lib/bijection')(),
+    assert = require('assert')
+    _ = require('underscore'),
+    alphabet = ('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ').split('');
+
+var i = 0;
+[0,1,2,3,4,5,6,7,8,9].forEach(function(item){
+  var encoded = base.encode(item);
+  assert.deepEqual(encoded, i.toString(), 
+    'Encoded value did not match expected.[encoded: ' + encoded + ' expected: ' + item + ']');
+  i++;
+});
+
+i=10;
+_.range(10, 61).forEach(function(item){
+  var encoded = base.encode(item);
+  assert.deepEqual(encoded, alphabet[i], 
+    'Encoded value did not match expected.[encoded: ' + encoded + ' expected: ' + alphabet[i] + ']');
+  i++;
+});
+
+assert.deepEqual(base.encode(62), '01', 'Expected 01, received: ' + base.encode(62));
+
+
+assert.deepEqual(base.encode(100), 'C1', 'Expected C1, received: ' + base.encode(100));
+assert.deepEqual(base.encode(10000), 'iB2', 'Expected iB2, received: ' + base.encode(10000));
+
