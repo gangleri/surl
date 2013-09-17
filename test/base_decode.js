a0b9705ee5f0d470e6bd2e82080bf8b46ae13273
gangleri <alan@gangleri.net>
2013-09-17 18:57:44 +0100

    removed base64 encoder and tests

--- test/base_decode.js
+++
@@ -1,7 +0,0 @@
-var base = require('../lib/base64'),
-    assert = require('assert');
-
-assert.deepEqual(base.decode('0'), 0);
-assert.deepEqual(base.decode('a'), 10);
-assert.deepEqual(base.decode('A'), 36);
-assert.deepEqual(base.decode('Z'), 61);
