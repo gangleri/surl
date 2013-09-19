6a36d519a263262ef30b7069e3a1ca1e3cd6ecd9
gangleri <alan@gangleri.net>
2013-09-19 23:53:05 +0100

    removed url validation tests

--- test/validation_isValidUrl.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert'),
-    isValidUrl = require('../lib/validation').isValidUrl;
-
-// valid urls
-assert.equal(isValidUrl(''), true, 'http url failed validation');
-
-// invalid urls
-assert.equal(isValidUrl(''), false, 'url cannot contain } character');
