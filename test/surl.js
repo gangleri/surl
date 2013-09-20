2db68571752791c5ef4b5e6f59ee5ee9a0e1714f
gangleri <alan@gangleri.net>
2013-09-20 23:09:34 +0100

    wrote tests using tap

--- test/surl.js
+++ test/surl.js
@@ -1,35 +1,51 @@
 var fs = require('fs');
 var rimraf = require('rimraf');
+var test = require('tap').test;
 
 if (fs.existsSync(__dirname + '/test-db')) rimraf.sync(__dirname + '/test-db');
 
 var surl = require('../index')(__dirname + '/test-db');
 var assert = require('assert');
 
-assert.equal(fs.existsSync(__dirname + '/test-db'), true, 'Failed to create test db');
-
-surl.shorten('http://github.com', function(err, shortId) {
-  assert.deepEqual(shortId, '0', 'Expected id[0] did not match actual.');
-  Test2();
+test('make sure the test database is created', function(t){
+  t.equal(fs.existsSync(__dirname + '/test-db'),true, 'test-db should have been created.');
+  t.end();
 });
 
-function Test2() { 
-  surl.shorten('http://google.com', function(err, shortId) {
-    assert.deepEqual(shortId, '1', 'Expected id[1] did not match actual.');
-    Test3();
-  });
-}
 
-function Test3() { 
-  surl.resolve('0', function(err, url) {
-    assert.deepEqual(url, 'http://github.com');
-    Test4();
-  });
-}
-
-function Test4() { 
+test('Insert first url into empty database', function(t){
   surl.shorten('http://github.com', function(err, shortId) {
-    assert.equal(shortId, '0', 'The same url should have generated the same shortId. Expected[0] Actual[' + shortId + ']')
+    t.equal(shortId, 0, 'Short id should be 0.');
+    t.equal(err, null, 'There should be no error');
+    t.end();
   });
-}
+  t.end();
+});
+
+test('Insert second url', function(t){
+  surl.shorten('http://google.com', function(err, shortId) {
+    t.equal(shortId, 1, 'Short id should be 1.');
+    t.equal(err, null, 'There should be no error');
+    t.end();
+  });
+  t.end();
+});
+
+test('Test resolving using first id', function(t){
+  surl.resolve('0', function(err, url) {
+    t.equal(url, 'http://github.com', '0 should resolve to http://github.com');
+    t.equal(err, null, 'There should be no error');
+    t.end();
+  });
+  t.end();
+});
+
+test('Attempting to shorten the same url should return existing short id', function(t){
+  surl.shorten('http://github.com', function(err, shortId) {
+    t.equal(shortId, 0, 'Short id should be 0.');
+    t.equal(err, null, 'There should be no error');
+    t.end();
+  });
+  t.end();
+});
 
